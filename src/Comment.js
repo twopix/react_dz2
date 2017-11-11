92df306a1e273ffbf7806bcda23325a69a031148
George Melidis <melidis@gmail.com>
2017-11-12 00:22:23 +0200

    second tests

--- src/Comment.js
+++ src/Comment.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 
-class App extends Component {
+class Comment extends Component {
   render() {
     return <div />;
   }
 }
 
-export default App;
+export default Comment;
--- src/NewsPost.js
+++ src/NewsPost.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 
-class App extends Component {
+class NewsPost extends Component {
   render() {
     return <div />;
   }
 }
 
-export default App;
+export default NewsPost;
